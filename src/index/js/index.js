af0e010f64f490dd2c177e8f7216e5c7fd6952fa
huangweiliang <905387498@qq.com>
2017-05-17 18:19:38 +0800

    加入轮播图

--- src/index/js/index.js
+++ src/index/js/index.js
@@ -3,4 +3,15 @@ $(function(){
   $(".menu").click(function() {
     $(".Menubar").slideToggle(500);
   })
+
+
+   var unslider04 = $('#b04').unslider({
+        dots: true
+    });
+    // data04 = unslider04.data('unslider');
+     
+    // $('.unslider-arrow04').click(function() {
+    //     var fn = this.className.split(' ')[1];
+    //     data04[fn]();
+    // });
 })

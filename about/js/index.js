07a3f9281b1fd307f3e8edb8b0b82c112d3cb2e7
huangweiliang <905387498@qq.com>
2017-05-18 18:17:13 +0800

    增加侧边栏

--- about/js/index.js
+++ about/js/index.js
@@ -1,1 +1,1 @@
-$(function(){$(".Menubar").css("display","none"),$(".menu").click(function(){$(".Menubar").slideToggle(500)})});
+$(function(){$(".Menubar").css("display","none"),$(".menu").click(function(){$(".Menubar").slideToggle(500)}),console.log($(".scrollTop")),$(".scrollTop").click(function(){isMoving=!0,$("html,body").stop().animate({scrollTop:0},500,function(){isMoving=!1})})});
--- index/js/index.js
+++ index/js/index.js
@@ -1,1 +1,1 @@
-$(function(){$(".Menubar").css("display","none"),$(".menu").click(function(){$(".Menubar").slideToggle(500)})});
+$(function(){$(".Menubar").css("display","none"),$(".menu").click(function(){$(".Menubar").slideToggle(500)});$(".in_banner").unslider({dots:!0,delay:5e3});console.log($(".scrollTop")),$(".scrollTop").click(function(){isMoving=!0,$("html,body").stop().animate({scrollTop:0},500,function(){isMoving=!1})})});
